f00f2e0467821bc7b98e7480ef48e459737e6437
Tokariew <tokariew@tokariew.xyz>
2025-06-22 11:20:28 +0200

    Python debugging

---
+++ lua/plugins/dap-python.lua
@@ -0,0 +1,76 @@
+return {
+  {
+    "mfussenegger/nvim-dap",
+    dependencies = {
+      "rcarriga/nvim-dap-ui",
+      { "theHamsta/nvim-dap-virtual-text", opts = {}, },
+    },
+    keys = {
+      { "<leader>db", function() require("dap").toggle_breakpoint() end, desc = "[D]ap [B]reakpoint" },
+      { "<leader>dc", function() require("dap").continue() end,          desc = "[D]ap [c]ontinue" },
+      { "<leader>do", function() require("dap").step_over() end,         desc = "[D]ap step [o]ver" },
+      { "<leader>di", function() require("dap").step_into() end,         desc = "[D]ap step [i]nto" },
+      { "<leader>dO", function() require("dap").step_out() end,          desc = "[D]ap step [O]ut" },
+      { "<leader>dq", function() require("dap").terminate() end,         desc = "[D]ap [Q]uit" },
+      -- { "<leader>du", function() require("dapui").toggle() end,          desc = "[D]ap toggle [U]i" }
+    },
+    config = function()
+      vim.fn.sign_define("DapBreakpoint", {
+        text = "",
+        texthl = "DiagnosticSignError",
+        linehl = "",
+        numhl = "",
+      })
+      vim.fn.sign_define("DapBreakpointRejected", {
+        text = "",
+        texthl = "DiagnosticSignError",
+        linehl = "",
+        numhl = "",
+      })
+      vim.fn.sign_define("DapStopped", {
+        text = "",
+        texthl = "DiagnosticSignWarn",
+        linehl = "Visual",
+        numhl = "DiagnosticSignWarn",
+      })
+
+      local vscode = require("dap.ext.vscode")
+      local json = require("plenary.json")
+      vscode.json_decode = function(str)
+        return vim.json.decode(json.json_strip_comments(str))
+      end
+    end
+  },
+
+  {
+    "rcarriga/nvim-dap-ui",
+    dependencies = {
+      "nvim-neotest/nvim-nio",
+    },
+    keys = {
+      { "<leader>du", function() require("dapui").toggle({}) end,  desc = "[D]ap [U]I" },
+      { "<leader>de", function() require("dapui").eval() end,      desc = "Dap [E]val", mode = { "n", "v" } },
+    },
+    opts = {},
+    config = function(_, opts)
+      local dap = require("dap")
+      local dapui = require("dapui")
+      dapui.setup(opts)
+      dap.listeners.after.event_initialized["dapui_config"] = function()
+        dapui.open({})
+      end
+      dap.listeners.before.event_terminated["dapui_config"] = function()
+        dapui.close({})
+      end
+      dap.listeners.before.event_exited["dapui_config"] = function()
+        dapui.close({})
+      end
+    end
+  },
+  {
+    "mfussenegger/nvim-dap-python",
+    config = function()
+      require("dap-python").setup(vim.fn.expand("~/.local/share") .. "/nvim/mason/bin/debugpy-adapter")
+    end
+  }
+}
