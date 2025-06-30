03db65c07bd15a00caf0f16b58d1d063c3ceaf71
Tokariew <tokariew@tokariew.xyz>
2025-06-30 20:08:51 +0200

    Fix partially broken snippets
    More info at https://github.com/Saghen/blink.cmp/issues/1944

--- lua/plugins/completion.lua
+++ lua/plugins/completion.lua
@@ -1,58 +1,71 @@
 return {
-  'saghen/blink.cmp',
-  dependencies = { 'rafamadriz/friendly-snippets',
-    'folke/lazydev.nvim' },
-
-  version = '1.*',
-  opts = {
-    sources = {
-      default = { 'lazydev', 'lsp', 'path', 'snippets', 'buffer' },
-      providers = {
-        lazydev = {
-          name = "LazyDev",
-          module = "lazydev.integrations.blink",
-          score_offset = 100,
+  {
+    "rafamadriz/friendly-snippets",
+    config = function()
+      require("luasnip.loaders.from_vscode").lazy_load()
+      require("luasnip.loaders.from_vscode").lazy_load({ paths = { vim.fn.stdpath("config") .. "/snippets" } })
+    end,
+  },
+  {
+    "saghen/blink.cmp",
+    dependencies = {
+      'folke/lazydev.nvim',
+      { 'L3MON4D3/LuaSnip', version = 'v2.*' },
+    },
+    version = '1.*',
+    opts = {
+      snippets = {
+        preset = "luasnip",
+      },
+      sources = {
+        default = { 'lazydev', 'lsp', 'path', 'snippets', 'buffer' },
+        providers = {
+          lazydev = {
+            name = "LazyDev",
+            module = "lazydev.integrations.blink",
+            score_offset = 100,
+          },
+        },
+      },
+      keymap = {
+        preset = "default",
+        ['<C-e>'] = { 'show_signature', 'hide_signature', 'fallback' },
+        ['<C-k>'] = { 'snippet_forward', 'fallback' },
+        ['<Tab>'] = false,
+        ['<S-Tab>'] = false,
+      },
+      signature = { enabled = true },
+      completion = {
+        documentation = {
+          auto_show = true,
+          auto_show_delay_ms = 500,
+        },
+        menu = {
+          draw = {
+            columns = { { "label", "label_description", gap = 1 }, { "kind_icon", gap = 1, "kind" }, },
+            components = {
+              kind_icon = {
+                text = function(ctx)
+                  local kind_icon, _, _ = require('mini.icons').get('lsp', ctx.kind)
+                  return kind_icon
+                end,
+                -- (optional) use highlights from mini.icons
+                highlight = function(ctx)
+                  local _, hl, _ = require('mini.icons').get('lsp', ctx.kind)
+                  return hl
+                end,
+              },
+              kind = {
+                -- (optional) use highlights from mini.icons
+                highlight = function(ctx)
+                  local _, hl, _ = require('mini.icons').get('lsp', ctx.kind)
+                  return hl
+                end,
+              }
+            }
+          }
         },
       },
     },
-    keymap = {
-      preset = 'default',
-      ['<C-e>'] =  { 'show_signature', 'hide_signature', 'fallback' },
-      ['<C-k>'] = { 'snippet_forward', 'fallback' },
-      ['<Tab>'] = false,
-      ['<S-Tab>'] = false,
-    },
-    signature = { enabled = true },
-    completion = {
-      documentation = {
-        auto_show = true,
-        auto_show_delay_ms = 500,
-      },
-      menu = {
-        draw = {
-          columns = { { "label", "label_description", gap = 1 }, { "kind_icon", gap = 1, "kind" }, },
-          components = {
-            kind_icon = {
-              text = function(ctx)
-                local kind_icon, _, _ = require('mini.icons').get('lsp', ctx.kind)
-                return kind_icon
-              end,
-              -- (optional) use highlights from mini.icons
-              highlight = function(ctx)
-                local _, hl, _ = require('mini.icons').get('lsp', ctx.kind)
-                return hl
-              end,
-            },
-            kind = {
-              -- (optional) use highlights from mini.icons
-              highlight = function(ctx)
-                local _, hl, _ = require('mini.icons').get('lsp', ctx.kind)
-                return hl
-              end,
-            }
-          }
-        }
-      }
-    }
-  }
+  },
 }
